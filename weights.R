f3f1e5d40fa0b0af049d071fb57ade24afc64907
chloevagnon <57488255+chloevagnon@users.noreply.github.com>
2021-03-04 17:30:42 +0100

    Update weights.R

--- weights.R
+++ weights.R
@@ -1,7 +1,8 @@
 weights<-function(prey_sizes,niche_center,niche_min,niche_max){
-  dnorm(prey_sizes,mean=niche_center,
+  W<-dnorm(prey_sizes,mean=niche_center,
       sd=sd(seq(from=niche_min,to=niche_max,
                 by=((niche_max-niche_min)/100))))/
   max(dnorm(prey_sizes,mean=niche_center,
             sd=sd(seq(from=niche_min,to=niche_max,
-                      by=((niche_max-niche_min)/100)))))}
+                      by=((niche_max-niche_min)/100)))))
+W/sum(W)}
